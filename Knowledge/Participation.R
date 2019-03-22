115644c4726d0943760fe2447765de98afce0333
Jennifer Lin <jennifer.lin16@ncf.edu>
2019-03-22 19:22:40 -0400

    Initiated Political knowledge R Project

---
+++ Knowledge/Participation.R
@@ -0,0 +1,13 @@
+#Political Participation
+#Jennifer Lin
+#ANES 2016 Time Series 
+
+#Load in data and change the name
+load("/Users/JenniferLin/Desktop/Working/ANES/DS0001/anes2016.rda")
+anes2016 = da36824.0001
+
+#Set the Working Directory
+getwd()
+setwd("~/Desktop/Working/Political-Prejudice/Knowledge")
+
+#Clean Political Participation Variables
