8b2c4a22db96d0d0244e8a98585e29014882d59a
Jennifer Lin <jennifer.lin16@ncf.edu>
2019-03-23 13:04:58 -0400

    Change data location file path

--- Knowledge/Participation.R
+++ Knowledge/Participation.R
@@ -3,7 +3,7 @@
 #ANES 2016 Time Series 
 
 #Load in data and change the name
-load("/Users/JenniferLin/Desktop/Working/ANES/DS0001/anes2016.rda")
+load("/Users/JenniferLin/Desktop/Data/ANES/DS0001/anes2016.rda")
 anes2016 = da36824.0001
 
 #Set the Working Directory
@@ -18,19 +18,22 @@ library(car)
 str(anes2016$V162010)
 table(anes2016$V162010)
 anes2016$talk = car::recode(anes2016$V162010, "-6:-9 = 'NA'")
-anes2016$talk <- as.numeric(as.factor(anes2016$V162010), na.rm = TRUE)
+anes2016$talk <- as.numeric(as.factor(anes2016$talk), na.rm = TRUE)
 str(anes2016$talk)
 
 #Go to political meeting - V162011
 str(anes2016$V162011)
 table(anes2016$V162011)
 anes2016$meeting = car::recode(anes2016$V162011, "-6:-9 = 'NA'" )
-anes2016$meeting = as.numeric(as.factor(anes2016$V162011))
+anes2016$meeting = as.numeric(as.factor(anes2016$meeting), na.rm = TRUE)
 str(anes2016$meeting)
 
 #Wear Campaign Button - V162012
 str(anes2016$V162012)
 table(anes2016$V162012)
+anes2016$wear = car::recode(anes2016$V162012, "-6:-9 = 'NA'")
+anes2016$wear = as.numeric(as.factor(anes2016$wear), na.rm = TRUE)
+str(anes2016$wear)
 
 #Do any other work for party - V162013
 str(anes2016$V162013)
@@ -56,7 +59,7 @@ table(anes2016$V162018B)
 str(anes2016$V162018E)
 table(anes2016$V162018E)
 
-#Contact US Representative or Senator
+#Contact US Representative or Senator - V162019
 str(anes2016$V162019)
 table(anes2016$V162019)
 
