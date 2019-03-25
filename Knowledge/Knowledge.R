a52bc665f199a2fc95bdb954f649e972f01567cd
Jennifer Lin <jennifer.lin16@ncf.edu>
2019-03-25 12:42:13 -0400

    Generated Knowledge graph

--- Knowledge/Knowledge.R
+++ Knowledge/Knowledge.R
@@ -84,3 +84,53 @@ table(anes2016$roberts) #Check before recoding values
 anes2016$roberts = car::recode(anes2016$roberts, "1 = 0")
 anes2016$roberts = car::recode(anes2016$roberts, "2 = 1")
 table(anes2016$roberts) #Check after and match to original distribution
+
+#Sum Political knowledge variables
+which( colnames(anes2016)=="biden" ) #1199
+anes2016$knowledge = anes2016[,1199]+anes2016[,1200]+anes2016[,1201]+anes2016[,1202]+anes2016[,1203]
+anes2016$knowledge
+summary(anes2016$knowledge, na.rm = TRUE)
+hist(anes2016$knowledge)
+
+#Declare Knowledge variable as categorical
+anes2016$knowledgecat = as.factor(as.numeric(anes2016$knowledge))
+str(anes2016$knowledgecat)
+table(anes2016$knowledgecat)
+
+library(forcats)
+anes2016$knowledgecat<- fct_explicit_na(anes2016$knowledgecat, na_level = "(Missing)")
+anes2016$knowledgecat = recode(anes2016$knowledgecat, "(Missing)" = "NA")
+table(anes2016$knowledgecat)
+
+#Descriptive Statistics for Knowledge by Party
+groups <- group_by(anes2016, party, knowledgecat)
+knowledge <- summarise(groups,
+                           mean = mean(knowledge, na.rm=TRUE),
+                           sd = sd(knowledge, na.rm=TRUE),
+                           n = n(),
+                           se=sd/sqrt(n),
+                           ci = qt(0.975,df=n-1)*se)
+
+knowledge
+
+#Remove the No Parties and Third Parties from chart 
+knowledge<-knowledge[!(knowledge$party=="NA"),]
+knowledge<-knowledge[!(knowledge$party=="No Party"),]
+knowledge<-knowledge[!(knowledge$party=="Other"),]
+
+#Remove NAs from Knowledge variable
+knowledge<-knowledge[!(knowledge$knowledgecat=="NA"),]
+
+#Make the Graph
+library(ggplot2)
+library(ggpubr)
+ggplot(knowledge, aes(x=knowledgecat, y=n, fill = party)) +
+  geom_bar(stat="identity", position=position_dodge()) + theme_classic()+
+  scale_fill_manual("Party ID", values = c("Democrat" = "blue", "Independent" = "purple", "Republican" = "red"))+
+  ggtitle("Political Knowledge by Party")+xlab("Total Political Knowledge")+ylab("Number of People")+
+  theme(text = element_text(size = 18, colour="black"),
+        axis.title = element_text(size = 20, colour="black"),
+        title = element_text(size = 24, colour="black"),
+        plot.title = element_text(hjust = 0.5))
+
+
