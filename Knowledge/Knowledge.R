3c85e40a1be0083dc3265779298a7c98efba9fcc
Jennifer Lin <jennifer.lin16@ncf.edu>
2019-03-25 12:28:31 -0400

    Added all recodes to knowledge variables

--- Knowledge/Knowledge.R
+++ Knowledge/Knowledge.R
@@ -42,9 +42,45 @@ anes2016$biden = car::recode(anes2016$biden, "2 = 1")
 table(anes2016$biden) #Check after and match to original distribution
 
 #V162073A - Ryan
+str(anes2016$V162073A)
+table(anes2016$V162073A) #Check original distribution
+anes2016$ryan = car::recode(anes2016$V162073A, "-6:-7 = 'NA'")
+anes2016$ryan = as.numeric(as.factor(anes2016$ryan))
+str(anes2016$ryan)
+table(anes2016$ryan) #Check before recoding values
+anes2016$ryan = car::recode(anes2016$ryan, "1 = 0")
+anes2016$ryan = car::recode(anes2016$ryan, "2 = 1")
+table(anes2016$ryan) #Check after and match to original distribution
 
-#V162074A - Merkel
+#V162074B - Merkel
+str(anes2016$V162074B)
+table(anes2016$V162074B) #Check original distribution
+anes2016$merkel = car::recode(anes2016$V162074B, "-6:-7 = 'NA'")
+anes2016$merkel = as.numeric(as.factor(anes2016$merkel))
+str(anes2016$merkel)
+table(anes2016$merkel) #Check before recoding values
+anes2016$merkel = car::recode(anes2016$merkel, "1 = 0")
+anes2016$merkel = car::recode(anes2016$merkel, "2 = 1")
+table(anes2016$merkel) #Check after and match to original distribution
 
 #V162075A - Putin
+str(anes2016$V162075B)
+table(anes2016$V162075B) #Check original distribution
+anes2016$putin = car::recode(anes2016$V162075B, "-6:-7 = 'NA'")
+anes2016$putin = as.numeric(as.factor(anes2016$putin))
+str(anes2016$putin)
+table(anes2016$putin) #Check before recoding values
+anes2016$putin = car::recode(anes2016$putin, "1 = 0")
+anes2016$putin = car::recode(anes2016$putin, "2 = 1")
+table(anes2016$putin) #Check after and match to original distribution
 
 #V162076B - Roberts
+str(anes2016$V162076B)
+table(anes2016$V162076B) #Check original distribution
+anes2016$roberts = car::recode(anes2016$V162076B, "-6:-7 = 'NA'")
+anes2016$roberts = as.numeric(as.factor(anes2016$roberts))
+str(anes2016$roberts)
+table(anes2016$roberts) #Check before recoding values
+anes2016$roberts = car::recode(anes2016$roberts, "1 = 0")
+anes2016$roberts = car::recode(anes2016$roberts, "2 = 1")
+table(anes2016$roberts) #Check after and match to original distribution
