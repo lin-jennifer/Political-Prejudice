e8ff6ceed6d507443e62dbb44bff33b94e48647c
Jennifer Lin <jennifer.lin16@ncf.edu>
2019-04-16 08:43:45 -0400

    Added Ideology codes

--- Knowledge/Feel2016.R
+++ Knowledge/Feel2016.R
@@ -45,3 +45,28 @@ table(anes2016$V162101)
 anes2016$feelcons = car::recode(anes2016$V162101, "-1:-99 = 'NA'")
 table(anes2016$feelcons)
 psych::describe(anes2016$feelcons, na.rm = TRUE)
+
+#V161158X - Party ID 7-category
+str(anes2016$V161158X)
+table(anes2016$V161158X)
+anes2016$pid7 = car::recode(anes2016$V161158X, "-8:-9 = 'NA'")
+table(anes2016$pid7)
+
+#V161155 - Party ID 3-point
+str(anes2016$V161155)
+table(anes2016$V161155)
+anes2016$pid3 = car::recode(anes2016$V161155, "-8:-9 = 'NA'")
+table(anes2016$pid3)
+
+#V162171 - Ideology 7-point
+str(anes2016$V162171)
+table(anes2016$V162171)
+anes2016$ideo7 = car::recode(anes2016$V162171, "-6:-9 = 'NA'")
+anes2016$ideo7 = car::recode(anes2016$ideo7, "99 = 'NA'")
+table(anes2016$ideo7)
+
+#V162171A - Ideology 3-point
+str(anes2016$V162171A)
+table(anes2016$V162171A)
+anes2016$ideo3 = car::recode(anes2016$V162171A, "-1:-9 = 'NA'")
+table(anes2016$ideo3)
