fe00c115499c03095af1eb5064d6926086089d1d
Jennifer Lin <jennifer.lin16@ncf.edu>
2019-04-20 10:48:45 -0400

    Created dataset for Methods Project

---
+++ RM-Project/Management.R
@@ -0,0 +1,33 @@
+#Management File
+#Jennifer Lin
+#Purpose: Create specific composite dataset from the "Knowledge" folder 
+#Main File - ANES 2016 Time Series 
+#Accessed: https://electionstudies.org/project/2016-time-series-study/
+
+#Run script files for Knowledge, Participation and Feeling2016 - IN THAT ORDER!!!!
+
+#Reset working directory to this project
+setwd("~/Desktop/Working/Political-Prejudice/RM-Project")
+
+#Political knowledge items
+which( colnames(anes2016)=="biden" ) #1199
+which( colnames(anes2016)=="roberts" ) #1203
+
+#Political Participation Variables
+which( colnames(anes2016)=="talk" ) #1204
+which( colnames(anes2016)=="vote16" ) #1214
+
+#Feeling Thermometer
+which( colnames(anes2016)=="feeldem" ) #1216
+which( colnames(anes2016)=="feelcons" ) #1219
+
+which( colnames(anes2016)=="pid3" ) #1197
+which( colnames(anes2016)=="party" ) #1198
+which( colnames(anes2016)=="pid7" ) #1220
+which( colnames(anes2016)=="ideo3" ) #1222
+
+composite <-anes2016[, c(2, 1197:1214, 1216:1222)]
+
+library(rio)
+export(composite, "RMProjData.csv")
+
