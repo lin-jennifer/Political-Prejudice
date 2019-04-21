fbe8636a77e03d1b28dcf567f0e26635424ce766
Jennifer Lin <jennifer.lin16@ncf.edu>
2019-04-20 22:10:30 -0400

    Edited opening notes

--- RM-Project/Management.R
+++ RM-Project/Management.R
@@ -1,10 +1,14 @@
-#Management File
-#Jennifer Lin
+###################################################
+#File: Management File
+#Author: Jennifer Lin
 #Purpose: Create specific composite dataset from the "Knowledge" folder 
-#Main File - ANES 2016 Time Series 
-#Accessed: https://electionstudies.org/project/2016-time-series-study/
+#Root data: ANES 2016 Time Series ( https://electionstudies.org/project/2016-time-series-study/)
+#Requires: `Knowledge.R`, `Participation.R` and `Feel2016.R` in `Knowledge` folder 
+#Git Repository: https://github.com/lin-jennifer/Political-Prejudice.git
+###################################################
 
 #Run script files for Knowledge, Participation and Feeling2016 - IN THAT ORDER!!!!
+#By doing so, you can get the variables that are needed to create the composite dataset.
 
 #Reset working directory to this project
 setwd("~/Desktop/Working/Political-Prejudice/RM-Project")
@@ -26,6 +30,8 @@ which( colnames(anes2016)=="party" ) #1198
 which( colnames(anes2016)=="pid7" ) #1220
 which( colnames(anes2016)=="ideo3" ) #1222
 
+#Find items needed for the study and build composite dataset for export
+#It is easier to work with a smaller, already cleaned dataset rather than something larger
 composite <-anes2016[, c(2, 1197:1214, 1216:1222)]
 
 library(rio)
