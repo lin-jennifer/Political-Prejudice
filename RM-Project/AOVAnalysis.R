e30e4b6b9ed9716d44a2cb322eb13df5d3ecd56b
Jennifer Lin <jennifer.lin16@ncf.edu>
2019-04-28 09:24:15 -0400

    Added KnowxPart split by party

--- RM-Project/AOVAnalysis.R
+++ RM-Project/AOVAnalysis.R
@@ -81,9 +81,50 @@ data$partfactor <- factor(data$part,
                              levels = c(1, 2, 3),
                              labels = c("Less", "Moderate", "More"))
 
+#Subsection data by political ideology identification
+lib.sub <- data[data$ideo3re=="Liberal",]
+con.sub <- data[data$ideo3re=="Conservative",]
+mod.sub <- data[data$ideo3re=="Moderate",]
 
-####################################################
-#Two way Anota for participation and knowledge on feelings
+#Subsection data by political party identification
+dem.sub <- data[data$party=="Democrat",]
+rep.sub <- data[data$party=="Republican",]
+ind.sub <- data[data$party=="Independent",]
+
+#Declare variables as factors for graphing purposes
+lib.sub$knowfactor <- factor(lib.sub$know, 
+                          levels = c(1, 2),
+                          labels = c("Less", "More"))
+
+lib.sub$partfactor <- factor(lib.sub$part, 
+                          levels = c(1, 2, 3),
+                          labels = c("Less", "Moderate", "More"))
+
+con.sub$knowfactor <- factor(con.sub$know, 
+                             levels = c(1, 2),
+                             labels = c("Less", "More"))
+
+con.sub$partfactor <- factor(con.sub$part, 
+                             levels = c(1, 2, 3),
+                             labels = c("Less", "Moderate", "More"))
+
+dem.sub$knowfactor <- factor(dem.sub$know, 
+                             levels = c(1, 2),
+                             labels = c("Less", "More"))
+
+dem.sub$partfactor <- factor(dem.sub$part, 
+                             levels = c(1, 2, 3),
+                             labels = c("Less", "Moderate", "More"))
+
+rep.sub$knowfactor <- factor(rep.sub$know, 
+                             levels = c(1, 2),
+                             labels = c("Less", "More"))
+
+rep.sub$partfactor <- factor(rep.sub$part, 
+                             levels = c(1, 2, 3),
+                             labels = c("Less", "Moderate", "More"))
+
+############### Participation x Feeling ANOVA (overall) #####################################
 
 #Feelings towards Democrats
 dem <- aov(feeldem ~ knowfactor*partfactor, data = data)
@@ -188,3 +229,218 @@ ggplot(cons.feel, aes(x=partfactor, y=mean, fill = knowfactor )) +
         title = element_text(size = 26, colour="black"),
         plot.title = element_text(hjust = 0.5))+
   scale_fill_manual("Knowledge", values = c("Less" = "firebrick1", "More" = "firebrick3"))
+
+################## Participation x Knowledge by Democrats #####################
+#This section contains feelings towards Democrats and Liberals sort by party (D or R) or ideology (L or C)
+
+#Feelings towards Democrats among Democrats
+dem <- aov(feeldem ~ knowfactor*partfactor, data = dem.sub)
+summary(dem)
+etaSquared(dem, anova = TRUE)
+
+#Comupte Cell means for feelings towards Democrats
+groups <- group_by(dem.sub, knowfactor, partfactor)
+dem.feel <- summarise(groups,
+                      mean = mean(feeldem, na.rm=TRUE),
+                      sd = sd(feeldem, na.rm=TRUE),
+                      n = n(),
+                      se=sd/sqrt(n),
+                      ci = qt(0.975,df=n-1)*se)
+dem.feel
+ggplot(dem.feel, aes(x=partfactor, y=mean, fill = knowfactor )) +
+  geom_bar(stat="identity", position=position_dodge()) +
+  geom_errorbar(aes(ymin=mean-ci, ymax=mean+ci), width=.2, size = 2, position=position_dodge(.9)) +
+  ggtitle("Knowledge and Participation among Democrats") +
+  xlab("Participation") +
+  ylab("Feelings towards Democrats") + ylim(0,100)+ theme_classic()+
+  theme(text = element_text(size = 22, colour="black"),
+        axis.title = element_text(size = 24, colour="black"),
+        title = element_text(size = 26, colour="black"),
+        plot.title = element_text(hjust = 0.5))+
+  scale_fill_manual("Knowledge", values = c("Less" = "blue", "More" = "blue4"))
+
+#Feelings towards Democrats among Republicans
+rep <- aov(feeldem ~ knowfactor*partfactor, data = rep.sub)
+summary(rep)
+etaSquared(rep, anova = TRUE)
+
+#Comupte Cell means for feelings towards Democrats
+groups <- group_by(rep.sub, knowfactor, partfactor)
+rep.feel <- summarise(groups,
+                      mean = mean(feeldem, na.rm=TRUE),
+                      sd = sd(feeldem, na.rm=TRUE),
+                      n = n(),
+                      se=sd/sqrt(n),
+                      ci = qt(0.975,df=n-1)*se)
+rep.feel
+ggplot(rep.feel, aes(x=partfactor, y=mean, fill = knowfactor )) +
+  geom_bar(stat="identity", position=position_dodge()) +
+  geom_errorbar(aes(ymin=mean-ci, ymax=mean+ci), width=.2, size = 2, position=position_dodge(.9)) +
+  ggtitle("Knowledge and Participation among Republicans") +
+  xlab("Participation") +
+  ylab("Feelings towards Democrats") + ylim(0,100)+ theme_classic()+
+  theme(text = element_text(size = 22, colour="black"),
+        axis.title = element_text(size = 24, colour="black"),
+        title = element_text(size = 26, colour="black"),
+        plot.title = element_text(hjust = 0.5))+
+  scale_fill_manual("Knowledge", values = c("Less" = "red", "More" = "red3"))
+
+#Feelings towards Liberals among Liberals
+lib <- aov(feellib ~ knowfactor*partfactor, data = lib.sub)
+summary(lib)
+etaSquared(lib, anova = TRUE)
+
+#Comupte Cell means for feelings towards Liberals
+groups <- group_by(lib.sub, knowfactor, partfactor)
+lib.feel <- summarise(groups,
+                      mean = mean(feellib, na.rm=TRUE),
+                      sd = sd(feellib, na.rm=TRUE),
+                      n = n(),
+                      se=sd/sqrt(n),
+                      ci = qt(0.975,df=n-1)*se)
+lib.feel
+ggplot(lib.feel, aes(x=partfactor, y=mean, fill = knowfactor )) +
+  geom_bar(stat="identity", position=position_dodge()) +
+  geom_errorbar(aes(ymin=mean-ci, ymax=mean+ci), width=.2, size = 2, position=position_dodge(.9)) +
+  ggtitle("Knowledge and Participation among Liberals") +
+  xlab("Participation") +ylim(0, 100)+
+  ylab("Feelings towards Liberals") + theme_classic()+
+  theme(text = element_text(size = 22, colour="black"),
+        axis.title = element_text(size = 24, colour="black"),
+        title = element_text(size = 26, colour="black"),
+        plot.title = element_text(hjust = 0.5))+
+  scale_fill_manual("Knowledge", values = c("Less" = "royalblue1", "More" = "royalblue3"))
+
+#Feelings towards Liberals among Conservatives
+cons <- aov(feellib ~ knowfactor*partfactor, data = con.sub)
+summary(cons)
+etaSquared(cons, anova = TRUE)
+
+#Comupte Cell means for feelings towards Conservatives
+groups <- group_by(con.sub, knowfactor, partfactor)
+cons.feel <- summarise(groups,
+                       mean = mean(feellib, na.rm=TRUE),
+                       sd = sd(feellib, na.rm=TRUE),
+                       n = n(),
+                       se=sd/sqrt(n),
+                       ci = qt(0.975,df=n-1)*se)
+cons.feel
+ggplot(cons.feel, aes(x=partfactor, y=mean, fill = knowfactor )) +
+  geom_bar(stat="identity", position=position_dodge()) +
+  geom_errorbar(aes(ymin=mean-ci, ymax=mean+ci), width=.2, size = 2, position=position_dodge(.9)) +
+  ggtitle("Knowledge and Participation among Conservatives") +
+  xlab("Participation") +ylim(0, 100)+
+  ylab("Feelings towards Liberals") + theme_classic()+
+  theme(text = element_text(size = 22, colour="black"),
+        axis.title = element_text(size = 24, colour="black"),
+        title = element_text(size = 26, colour="black"),
+        plot.title = element_text(hjust = 0.5))+
+  scale_fill_manual("Knowledge", values = c("Less" = "firebrick1", "More" = "firebrick3"))
+
+################### Knowledge x Participation for Republicans #####################
+#This section contains feelings towards Republicans and Conservatives sort by party (D or R) or ideology (L or C)
+
+#Feelings towards Republicans among Democrats
+dem <- aov(feelrep ~ knowfactor*partfactor, data = dem.sub)
+summary(dem)
+etaSquared(dem, anova = TRUE)
+
+#Comupte Cell means for feelings towards Democrats
+groups <- group_by(dem.sub, knowfactor, partfactor)
+dem.feel <- summarise(groups,
+                      mean = mean(feelrep, na.rm=TRUE),
+                      sd = sd(feelrep, na.rm=TRUE),
+                      n = n(),
+                      se=sd/sqrt(n),
+                      ci = qt(0.975,df=n-1)*se)
+dem.feel
+ggplot(dem.feel, aes(x=partfactor, y=mean, fill = knowfactor )) +
+  geom_bar(stat="identity", position=position_dodge()) +
+  geom_errorbar(aes(ymin=mean-ci, ymax=mean+ci), width=.2, size = 2, position=position_dodge(.9)) +
+  ggtitle("Knowledge and Participation among Democrats") +
+  xlab("Participation") +
+  ylab("Feelings towards Republicans") + ylim(0,100)+ theme_classic()+
+  theme(text = element_text(size = 22, colour="black"),
+        axis.title = element_text(size = 24, colour="black"),
+        title = element_text(size = 26, colour="black"),
+        plot.title = element_text(hjust = 0.5))+
+  scale_fill_manual("Knowledge", values = c("Less" = "blue", "More" = "blue4"))
+
+#Feelings towards Democrats among Republicans
+rep <- aov(feelrep ~ knowfactor*partfactor, data = rep.sub)
+summary(rep)
+etaSquared(rep, anova = TRUE)
+
+#Comupte Cell means for feelings towards Democrats
+groups <- group_by(rep.sub, knowfactor, partfactor)
+rep.feel <- summarise(groups,
+                      mean = mean(feelrep, na.rm=TRUE),
+                      sd = sd(feelrep, na.rm=TRUE),
+                      n = n(),
+                      se=sd/sqrt(n),
+                      ci = qt(0.975,df=n-1)*se)
+rep.feel
+ggplot(rep.feel, aes(x=partfactor, y=mean, fill = knowfactor )) +
+  geom_bar(stat="identity", position=position_dodge()) +
+  geom_errorbar(aes(ymin=mean-ci, ymax=mean+ci), width=.2, size = 2, position=position_dodge(.9)) +
+  ggtitle("Knowledge and Participation among Republicans") +
+  xlab("Participation") +
+  ylab("Feelings towards Republicans") + ylim(0,100)+ theme_classic()+
+  theme(text = element_text(size = 22, colour="black"),
+        axis.title = element_text(size = 24, colour="black"),
+        title = element_text(size = 26, colour="black"),
+        plot.title = element_text(hjust = 0.5))+
+  scale_fill_manual("Knowledge", values = c("Less" = "red", "More" = "red3"))
+
+#Feelings towards Liberals among Liberals
+lib <- aov(feelcons ~ knowfactor*partfactor, data = lib.sub)
+summary(lib)
+etaSquared(lib, anova = TRUE)
+
+#Comupte Cell means for feelings towards Liberals
+groups <- group_by(lib.sub, knowfactor, partfactor)
+lib.feel <- summarise(groups,
+                      mean = mean(feelcons, na.rm=TRUE),
+                      sd = sd(feelcons, na.rm=TRUE),
+                      n = n(),
+                      se=sd/sqrt(n),
+                      ci = qt(0.975,df=n-1)*se)
+lib.feel
+ggplot(lib.feel, aes(x=partfactor, y=mean, fill = knowfactor )) +
+  geom_bar(stat="identity", position=position_dodge()) +
+  geom_errorbar(aes(ymin=mean-ci, ymax=mean+ci), width=.2, size = 2, position=position_dodge(.9)) +
+  ggtitle("Knowledge and Participation among Liberals") +
+  xlab("Participation") +ylim(0, 100)+
+  ylab("Feelings towards Conservatives") + theme_classic()+
+  theme(text = element_text(size = 22, colour="black"),
+        axis.title = element_text(size = 24, colour="black"),
+        title = element_text(size = 26, colour="black"),
+        plot.title = element_text(hjust = 0.5))+
+  scale_fill_manual("Knowledge", values = c("Less" = "royalblue1", "More" = "royalblue3"))
+
+#Feelings towards Liberals among Conservatives
+cons <- aov(feelcons ~ knowfactor*partfactor, data = con.sub)
+summary(cons)
+etaSquared(cons, anova = TRUE)
+
+#Comupte Cell means for feelings towards Conservatives
+groups <- group_by(con.sub, knowfactor, partfactor)
+cons.feel <- summarise(groups,
+                       mean = mean(feelcons, na.rm=TRUE),
+                       sd = sd(feelcons, na.rm=TRUE),
+                       n = n(),
+                       se=sd/sqrt(n),
+                       ci = qt(0.975,df=n-1)*se)
+cons.feel
+ggplot(cons.feel, aes(x=partfactor, y=mean, fill = knowfactor )) +
+  geom_bar(stat="identity", position=position_dodge()) +
+  geom_errorbar(aes(ymin=mean-ci, ymax=mean+ci), width=.2, size = 2, position=position_dodge(.9)) +
+  ggtitle("Knowledge and Participation among Conservatives") +
+  xlab("Participation") +ylim(0, 100)+
+  ylab("Feelings towards Conservatives") + theme_classic()+
+  theme(text = element_text(size = 22, colour="black"),
+        axis.title = element_text(size = 24, colour="black"),
+        title = element_text(size = 26, colour="black"),
+        plot.title = element_text(hjust = 0.5))+
+  scale_fill_manual("Knowledge", values = c("Less" = "firebrick1", "More" = "firebrick3"))
+
